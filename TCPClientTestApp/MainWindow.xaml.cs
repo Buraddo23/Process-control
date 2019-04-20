c054c9640b55d872c0f3767a13879e26d9798d8b
Vlad Sîngeorzan <vlad.singeorzan@student.upt.ro>
2019-04-20 13:45:37 +0300

    adaugare simulator

--- TCPClientTestApp/MainWindow.xaml.cs
+++ TCPClientTestApp/MainWindow.xaml.cs
@@ -15,8 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Simulator;
 
-namespace TCPClientTestApp
+namespace TCPMonitor
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
@@ -29,73 +30,100 @@ namespace TCPClientTestApp
 
 		public MainWindow()
 		{
-			InitializeComponent();
-		}
-
-		private void Listen()
-		{
-			TcpListener server = null;
-			try
-			{
-				Int32 port = 3000;
-				IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-
-				server = new TcpListener(localAddr, port);
-
-				//Start listening for client requests.
-				server.Start();
-
-				//Buffer for reading data
-				Byte[] bytes = new Byte[8];
-				String data = null;
+			InitializeComponent();            
+        }
 
 
-				//Enter the listening loop.
-				while (true)
-				{
-					bck.ReportProgress(0, "Waiting for a connection... ");
 
-					//Accept TcpClient
-					TcpClient client = server.AcceptTcpClient();
-					bck.ReportProgress(0, "Connected!");
-					
+        private void btnListen_Click(object sender, RoutedEventArgs e)
+        {
+            tbDataReceived.Text = "Start Listening";
+            bck = new BackgroundWorker();
+            bck.WorkerReportsProgress = true;
+            bck.ProgressChanged += Bck_ProgressChanged;
+            bck.DoWork += Bck_DoWork;
+            bck.RunWorkerAsync();
+        }
 
-					data = null;
+        private void Bck_DoWork(object sender, DoWorkEventArgs e)
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 3000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-					stream = client.GetStream();
+                server = new TcpListener(localAddr, port);
 
-					int i;
+                //Start listening for client requests.
+                server.Start();
 
-					//Get all data sent by the client.
-					while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-					{
-						//display received data by reporting progress to the background worker
-						bck.ReportProgress(0, bytes[1].ToString() + ", " + bytes[6].ToString());					
-					}
+                //Buffer for reading data
+                Byte[] bytes = new Byte[8];
 
-					//Shutdown and end connection
-					client.Close();
-				}
+                //Enter the listening loop.
+                while (true)
+                {
+                    bck.ReportProgress(0, "Waiting for a connection... ");
 
-			}
-			catch (Exception ex)
-			{
-				bck.ReportProgress(0, string.Format("SocketException: {0}", ex.ToString()));
-			}
-		}
+                    //Accept TcpClient
+                    TcpClient client = server.AcceptTcpClient();
+                    bck.ReportProgress(0, "Connected!");
 
+
+                    stream = client.GetStream();
+
+                    int i;
+
+                    //Get all data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        //display received data by reporting progress to the background worker
+                        bck.ReportProgress(0, bytes[1].ToString() + ", " + bytes[6].ToString());
+                    }
+
+                    //Shutdown and end connection
+                    client.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                bck.ReportProgress(0, string.Format("SocketException: {0}", ex.ToString()));
+            }
+        }
+
+        private void Bck_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string data = (string)e.UserState;
+
+            tbDataReceived.Text = string.Format("Received: {0}", data) + Environment.NewLine + tbDataReceived.Text;
+        }
+
+        private void SendMessage(int Message)
+        {
+            //change IP address to the machine where you want to send the message to
+            if(client==null)
+            {
+                client = new TcpClient("127.0.0.1", 2000);
+            }
+            
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = new byte[8];           
+            bytesToSend[1] = (byte)Message;
+
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+        /// <summary>
+        /// /// eveniment folosit pentru a trimite o comanda catre proces/simulator
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 		private void btnSend_Click(object sender, RoutedEventArgs e)
 		{	try
-			{	
-				if (client == null)
-					CreateConnection();
-				
-				NetworkStream nwStream = client.GetStream();
-				byte[] bytesToSend = new byte[8];
-				bytesToSend[1] = 1;
-				bytesToSend[6] = 2;
-								
-				nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+			{
+                SendMessage((int)Command.LongDelay | (int)Command.PumpOne | (int)Command.PumpTwo | (int)Command.Started);             
 			}
 			catch (Exception ex)
 			{
@@ -103,32 +131,24 @@ namespace TCPClientTestApp
 			}
 		}
 
-		private void btnListen_Click(object sender, RoutedEventArgs e)
-		{
-			tbDataReceived.Text = "Start Listening";
-			bck = new BackgroundWorker();
-			bck.WorkerReportsProgress = true;
-			bck.ProgressChanged += Bck_ProgressChanged;
-			bck.DoWork += Bck_DoWork;
-			bck.RunWorkerAsync();			
-		}
+		
 
-		private void Bck_DoWork(object sender, DoWorkEventArgs e)
-		{
-			Listen();
-		}
-
-		private void Bck_ProgressChanged(object sender, ProgressChangedEventArgs e)
-		{
-			string data = (string)e.UserState;
-			
-			tbDataReceived.Text = tbDataReceived.Text + Environment.NewLine + string.Format("Received: {0}", data);			
-		}
-
-		private void CreateConnection()
-		{
-			//change IP address to the machine where you want to send the message to
-			client = new TcpClient("127.0.0.1", 2000);
-		}
-	}
+        /// <summary>
+        /// eveniment folosit pentru a trimite o comanda catre proces/simulator
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SendMessage((int)Command.SmallDelay | (int)Command.PumpOne | (int)Command.PumpTwo | (int)Command.Stopped);               
+            }
+            catch (Exception ex)
+            {
+                tbDataReceived.Text = tbDataReceived + ex.ToString();
+            }
+        }
+      
+    }
 }
